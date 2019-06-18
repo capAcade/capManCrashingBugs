a441ec87efe37af65ae455ae1d92317971a240c9
Sven van Straalen <ssgvanstraalen@gmail.com>
2019-06-18 09:18:09 +0200

    widescreen changes

--- src/js/states/update.js
+++ src/js/states/update.js
@@ -2,7 +2,7 @@ export default function()  {
     this._handleCollisions();
     this._handleInput();
     this.coinFont.text = `x${this.scorep1}`;
-    this.game.camera.focusOnXY(this.hero.x, this.hero.y - 400)
+    this.game.camera.focusOnXY(this.hero.x, this.hero.y )
     this.keyIcon.frame = this.hasKey ? 1 : 0;
     this.door.frame = this.hasKey ? 1 : 0;
     //this.game.camera.x = this.hero.x -40;
